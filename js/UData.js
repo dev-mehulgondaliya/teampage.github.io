cb7f00ded1f7e2711901df92ffa3a29a4c942a38
Mehul Gondaliya <137489580+dev-mehulgondaliya@users.noreply.github.com>
2023-08-26 11:17:21 +0530

    responsive team page

---
+++ js/UData.js
@@ -0,0 +1,12 @@
+export const universityData = [
+  "cambridge university",
+  "Harvard University",
+  "University of California",
+  "ETH Zürich",
+  "Max Planck Society",
+  "The University of Edinburgh",
+  "Johns Hopkins University",
+  "University of Illinois",
+  "Humboldt University of Berlin",
+  "Cornell University",
+];
---
+++ js/mData.js
@@ -0,0 +1,47 @@
+export const mentorData = [
+  {
+    name: "Elina Smith",
+    universityname: "cambridge university",
+    profileimg: "images/mentor/1.jpg",
+  },
+  {
+    name: "john Cartle",
+    universityname: "Max Planck Society",
+    profileimg: "images/mentor/2.jpg",
+  },
+  {
+    name: "Zuc zing",
+    universityname: "University of Illinois",
+    profileimg: "images/mentor/3.jpg",
+  },
+  {
+    name: "Farid lord",
+    universityname: "Cornell University",
+    profileimg: "images/mentor/4.jpg",
+  },
+  {
+    name: "Elizabeth Martin",
+    universityname: "University of California",
+    profileimg: "images/mentor/5.jpg",
+  },
+  {
+    name: "Eleanor Williams",
+    universityname: "cambridge university",
+    profileimg: "images/mentor/6.jpg",
+  },
+  {
+    name: "Isabella Anderson",
+    universityname: "ETH Zürich",
+    profileimg: "images/mentor/7.jpg",
+  },
+  {
+    name: "Emma Garcia",
+    universityname: "cambridge university",
+    profileimg: "images/mentor/8.jpg",
+  },
+  {
+    name: "James Davis",
+    universityname: "ETH Zürich",
+    profileimg: "images/mentor/9.jpg",
+  },
+];
